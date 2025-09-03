9a04c6c4bcd7d0a4ebbb4881024144bf17eabdb7
sonukumar1S <sonukumarcs2003@gmail.com>
2025-09-03 11:40:46 +0530

    first commit

---
+++ Day1/JavaBasics.java
@@ -0,0 +1,50 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        // System.out.println("Hello Java");
+        // System.out.println("Hello Java");
+        // System.out.println("Hello Java");
+
+        // System.out.println("* * * *");
+        // System.out.println("* * *");
+        // System.out.println("* *");
+        // System.out.println("*");
+
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a);
+        // System.out.println(b);
+        // String name = "Tony Stark";
+        // System.out.println(name);
+
+
+        // a = 100;
+        // a = b;
+        // System.out.println(a);
+
+        // byte b = 8;
+        // System.out.println(b);
+
+        // char ch = 'a';
+        // System.out.println(ch);
+
+        // boolean var = true;
+        // System.out.println(var);
+
+        // float price = 10.5f;
+        // System.out.println(price);
+
+        // int number = 25;
+        // System.out.println(number);
+        
+        // // long
+        // // double
+
+        // short num = 240;
+        // System.out.println(num);
+
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
