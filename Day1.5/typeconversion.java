a6978d5e620f311fbada6bd9af3ced40cd19e282
sonukumar1S <sonukumarcs2003@gmail.com>
2025-09-06 12:16:12 +0530

    Java Programming Language

---
+++ Day1.5/typeconversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class typeconversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextInt();
+        System.out.println(number);
+
+        int a = 25;
+        long b = a;
+        System.out.println(b);
+    }
+}
---
+++ Day1.6/typeCasting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class typeCasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // float a = 25.9999f;
+        // int b = (int)a;
+        // System.out.println(b);
+
+        float marks = 99.999f;
+        int marks2 = (int) marks;
+        System.out.println(marks2);
+    }
+}
